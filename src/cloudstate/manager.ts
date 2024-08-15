ae2660b42c195e3181397cceca5e73104c1fd697
Benjamin Swerdlow <Swerdlowbenjamin@gmail.com>
2024-08-15 10:45:24 -0700

    try this

--- src/cloudstate/manager.ts
+++ src/cloudstate/manager.ts
@@ -3,9 +3,11 @@ import { ChatCS } from "./chatbot";
 
 @cloudstate
 export class ConversationManagerCS {
-  static id = "conversation-manager";
+  static readonly id = "conversation-manager";
   conversations = new Map<string, ChatCS>();
-
+  doesConversationExist(conversationId: string) {
+    return this.conversations.has(conversationId);
+  }
   async createConversation() {
     const conversation = new ChatCS();
     this.conversations.set(conversation.id, conversation);
