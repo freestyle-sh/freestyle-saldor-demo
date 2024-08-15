f2cc34a66a194fba66e17e666df285797f1d687d
Benjamin Swerdlow <Swerdlowbenjamin@gmail.com>
2024-08-14 23:14:26 -0700

    upgrades

--- freestyle.config.ts
+++ freestyle.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "freestyle-sh";
-
+import * as dotenv from 'dotenv';
+dotenv.config();
 export default defineConfig({
   dev: {
     command: "npx astro dev",
--- src/cloudstate/chatbot.ts
+++ src/cloudstate/chatbot.ts
@@ -1,52 +1,88 @@
 import { cloudstate, useRequest } from "freestyle-sh";
 import { MessageListCS, TextMessageCS } from "freestyle-chat";
 import type { BaseUserCS } from "freestyle-auth/passkey";
-import axios from "axios";
 import { parse as parseCookie } from "cookie";
 import { OpenAIEmbeddings, ChatOpenAI } from "@langchain/openai";
-import { MemoryVectorStore} from "langchain/vectorstores/memory";
+import { MemoryVectorStore } from "langchain/vectorstores/memory";
 import { createStuffDocumentsChain } from "langchain/chains/combine_documents";
 import { Document } from "@langchain/core/documents";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 import { StringOutputParser } from "@langchain/core/output_parsers";
-import { PromptTemplate } from '@langchain/core/prompts';
-
+import { PromptTemplate } from "@langchain/core/prompts";
+import humanizeDuration from "humanize-duration";
 export class SaldorClient {
-  client;
+  apiKey: string;
+  baseURL: string;
 
-  constructor(apiKey: string, baseURL = 'http://localhost:8000') {
-      this.client = axios.create({
-          baseURL,
-          timeout: 50000,
-          headers: {
-              'x-api-key': apiKey,
-          },
-      });
+  constructor(apiKey: string, baseURL = "https://api.saldor.com") {
+    // this.client = axios.create({
+    //     baseURL,
+    //     timeout: 50000,
+    //     headers: {
+    //         'x-api-key': apiKey,
+    //     },
+    // });
+    this.apiKey = apiKey;
+    this.baseURL = baseURL;
   }
 
   // Example method to scrape data
   async crawl(url: string, goal: string) {
-  try {
-          const response = await this.client.post('/crawl', {
-          url, 
+    try {
+      const response = await fetch(`${this.baseURL}/crawl`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "x-api-key": this.apiKey,
+        },
+        body: JSON.stringify({
+          url,
           goal,
           max_depth: 0,
-          max_pages: 100,
-  });
-      return response.data
+          max_pages: 10,
+        }),
+      });
+      return response.json() as Promise<{
+        data: {
+          id: string;
+          state: string;
+          result: string[];
+        }
+      }>;;
+      //         const response = await this.client.post('/crawl', {
+      //         url,
+      //         goal,
+      //         max_depth: 0,
+      //         max_pages: 100,
+      // });
+      //     return response.data
 
-  }
-
-  catch (error) {
-          // Handle error appropriately
-          console.error('Scrape failed:', error);
-          throw error;
-      }
+      // }
+    } catch (error) {
+      // Handle error appropriately
+      console.error("Scrape failed:", error);
+      throw error;
+    }
   }
 
   async get_crawl_status(crawl_id: string) {
-      const response = await this.client.get(`/crawl/${crawl_id}`);
-      return response.data
+    // const response = await this.client.get(`/crawl/${crawl_id}`);
+    const response = await fetch(`${this.baseURL}/crawl/${crawl_id}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "x-api-key": this.apiKey,
+      }
+    })
+    
+    return response.json() as Promise<{
+      data: {
+        id: string;
+        state: string;
+        result: string[];
+      }
+    }>;
+    // return response.data;
   }
 }
 
@@ -59,11 +95,9 @@ export class ChatCS extends MessageListCS {
     retriever: undefined,
   } as {
     lastCached: number;
-    chain: any; 
+    chain: any;
     retriever: any;
   };
-  
-
 
   async saldorCall() {
     const refetchTime = 1000 * 60 * 60 * 24; // 24 hours
@@ -71,29 +105,40 @@ export class ChatCS extends MessageListCS {
       return this.ragCache.chain, this.ragCache.retriever;
     }
 
-    const saldor = new SaldorClient(process.env.SALDOR_API_KEY);
-    const out = await saldor.crawl("https://techcrunch.com/category/startups/", "Acquire all information from techcrunch.com relating to startup news");
+
+    const saldor = new SaldorClient(process.env.SALDOR_API_KEY!);
+    const out = await saldor.crawl(
+      "https://techcrunch.com/category/startups/",
+      "Acquire all information from techcrunch.com relating to startup news"
+    );
     const crawlId = out.data.id;
     let crawlStatus;
+    let crawlStarted = Date.now();
     do {
-        crawlStatus = await saldor.get_crawl_status(crawlId);
-        console.log("Current crawl status:", crawlStatus.data.state);
-        if (crawlStatus.data.state == 'running' || crawlStatus.data.state == 'pending') {
-            // Wait for 5 seconds before checking again
-            await new Promise(resolve => setTimeout(resolve, 5000));
-        }
-    } while (crawlStatus.data.state == 'running' || crawlStatus.data.state == 'pending');
-    
-    if (crawlStatus.data.state == 'completed') {
-        console.log("Crawl completed successfully");
-        console.log(crawlStatus.data);
+      crawlStatus = await saldor.get_crawl_status(crawlId);
+      console.log("Current crawl status:", crawlStatus.data.state, humanizeDuration(Date.now() - crawlStarted));
+      if (
+        crawlStatus.data.state == "running" ||
+        crawlStatus.data.state == "pending"
+      ) { 
+        // Wait for 5 seconds before checking again
+        await new Promise((resolve) => setTimeout(resolve, 5000));
+      }
+    } while (
+      crawlStatus.data.state == "running" ||
+      crawlStatus.data.state == "pending"
+    );
+
+    if (crawlStatus.data.state == "completed") {
+      console.log("Crawl completed successfully");
+      console.log(crawlStatus.data);
     } else {
       console.error("Crawl failed");
-      return
+      return;
     }
-    const results: string[] = crawlStatus.data.result
+    const results: string[] = crawlStatus.data.result;
 
-    const docs = results.map(item => new Document({ pageContent: item }));
+    const docs = results.map((item) => new Document({ pageContent: item }));
     const textSplitter = new RecursiveCharacterTextSplitter({
       chunkSize: 1000,
       chunkOverlap: 200,
@@ -110,7 +155,7 @@ export class ChatCS extends MessageListCS {
 
     // Define your prompt template
     const promptTemplate = new PromptTemplate({
-      inputVariables: ['context', 'question'],
+      inputVariables: ["context", "question"],
       template: `
       You are a knowledgeable assistant. Given the following context:
   
@@ -120,16 +165,15 @@ export class ChatCS extends MessageListCS {
   
       {question}
     `,
-  });
+    });
 
+    const llm = new ChatOpenAI({ model: "gpt-3.5-turbo", temperature: 0 });
 
-  const llm = new ChatOpenAI({ model: "gpt-3.5-turbo", temperature: 0 });
-  
-  const ragChain = await createStuffDocumentsChain({
-    llm,
-    prompt: promptTemplate,
-    outputParser: new StringOutputParser(),
-  });
+    const ragChain = await createStuffDocumentsChain({
+      llm,
+      prompt: promptTemplate,
+      outputParser: new StringOutputParser(),
+    });
 
     this.ragCache.chain = ragChain;
     this.ragCache.retriever = retriever;
@@ -151,7 +195,7 @@ export class ChatCS extends MessageListCS {
   override async _onMessageAdded(message: TextMessageCS): Promise<void> {
     try {
       if (message.sender.id !== "SALDOR") {
-        const chain, retriever = await this.saldorCall();
+        const [chain, retriever] = await this.saldorCall();
         const retrieved_docs = await retriever.invoke(message.text);
         let data = await chain.invoke({
           context: retriever,
@@ -161,13 +205,15 @@ export class ChatCS extends MessageListCS {
           data = "No data found";
         }
         // rag goes here
-        await this._addMessage(new TextMessageCS({
-          text: data,
-          sender: {
-            id: "SALDOR",
-            username: "Saldor",
-          },
-        }))
+        await this._addMessage(
+          new TextMessageCS({
+            text: data,
+            sender: {
+              id: "SALDOR",
+              username: "Saldor",
+            },
+          })
+        );
       }
     } catch (e) {
       console.error(e);
